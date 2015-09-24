c75b6b2e4637f3b56184bc94f1eca7a31756b4a6
dqi12thuermer <dqi12thuermer@W101_69.szut.uni-bremen.de>
2015-09-24 08:44:24 +0200

    server edit

--- Friends-Hug/src/model/Server.java
+++ Friends-Hug/src/model/Server.java
@@ -1,10 +1,41 @@
 package model;
 
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 
 public class Server {
 
 	private HashMap<Integer,ServerThread> clientlist;
 	
+	private InetAddress ip;
 
+	public InetAddress getIp() {
+		return ip;
+	}
+
+	public void setIp() {
+		try {
+			this.ip = InetAddress.getLocalHost();
+		}
+		catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			System.out.println("keine IP-Adresse gefunden!");
+		}
+	}
+		
+	public void start() throws java.net.SocketException, java.io.IOException{
+		int i= 0;
+		try{
+			ServerSocket serverSocket = new ServerSocket(7777,0,getIp());
+			while(true){
+				Socket cs = serverSocket.accept();
+			}
+		}
+		catch(Exception e){
+			
+		}
+	}
 }
