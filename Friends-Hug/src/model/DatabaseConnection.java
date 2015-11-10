92d37923489621f6451994549a0e515d87d50540
mel97 <melina.wolle@gmail.com>
2015-11-10 08:53:23 +0100

    Database connection

--- Friends-Hug/src/model/DatabaseConnection.java
+++ Friends-Hug/src/model/DatabaseConnection.java
@@ -1,5 +1,35 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;  
+import java.sql.ResultSet;  
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseConnection {
-//fdhgd
+	public void Connection(){
+		Connection connection = null;
+		ResultSet resultSet = null;
+		Statement statement =null;
+		
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:db/FriendsHug.db3");
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("Select * from Data");
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				resultSet.close();
+				statement.close();
+				connection.close();
+			} 
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
