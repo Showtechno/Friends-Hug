bbfe7ec29a33c3ab5378392c311dfbc8f8ea588b
dqi12thuermer <dqi12thuermer@W127_04.szut.uni-bremen.de>
2015-11-24 09:26:53 +0100

    nameChange and methods update

---
+++ Friends-Hug/src/view/ChatfensterChanges.java
@@ -0,0 +1,10 @@
+package view;
+
+public interface ChatfensterChanges {
+
+	public String nachrichtenFensterChange();
+	
+	public Boolean serverStatusChange();
+	
+	public String teilnehmerChange();
+}
