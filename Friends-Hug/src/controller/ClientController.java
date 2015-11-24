21023728771f6ed02dd8a4aefcefdf7776f13df0
dqi12thuermer <dqi12thuermer@W127_04.szut.uni-bremen.de>
2015-11-24 09:09:56 +0100

    Singelton Pattern but Class not needed (at moment) no VMC

--- Friends-Hug/src/controller/ClientController.java
+++ Friends-Hug/src/controller/ClientController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import java.util.Observable;
+import java.util.Observer;
+
+import model.Client;
+import view.Chatfenster;
+
 public class ClientController {
 
+	private static ClientController instance = new ClientController();
+	
+	public static ClientController getInstance(){
+		return instance;
+	}	
 }
