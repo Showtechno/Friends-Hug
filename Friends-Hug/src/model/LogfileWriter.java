2b737581ef1a81f95da805d1fb21c530be18fca3
Showtech <tvhamster4@hotmail.de>
2016-02-10 17:41:42 +0100

    logfilewriter fix

--- Friends-Hug/src/model/LogfileWriter.java
+++ Friends-Hug/src/model/LogfileWriter.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.sql.Timestamp;
@@ -7,19 +8,26 @@ import java.text.SimpleDateFormat;
 
 public class LogfileWriter {
 	
-	
-	File logDatei;
-	FileWriter writer;
 	static SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss.SSS");
 	static Timestamp time = new Timestamp(System.currentTimeMillis());
 	static String dateTime = format.format(time);
-
+	private static LogfileWriter instance;
+	
+	public static LogfileWriter getInstance() {
+		if (instance == null) {
+			instance = new LogfileWriter();
+		}
+		return instance;
+	}
+		
 	public void writeLogfile(String input){
 		
-		logDatei = new File("Log.text");
 		try{
-			writer = new FileWriter(logDatei, true);
-			writer.write(dateTime + ": " + input + "\n");
+			FileWriter fWriter = new FileWriter("log.txt");
+			BufferedWriter bWriter = new BufferedWriter(fWriter);
+			bWriter.write(dateTime + ": " + input + "\n");
+			System.out.println("Ereignis wurde in die Log.txt geschrieben.");
+			bWriter.close();
 		}
 		catch(Exception e){
 			System.out.println("ACHTUNG: Logfile konnte nicht beschrieben werden!");
