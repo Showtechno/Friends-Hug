d8b767b365e3a3acd6a1485d0e5051eee58cc287
dqi12thuermer <dqi12thuermer@W129_03.szut.uni-bremen.de>
2016-02-15 10:00:13 +0100

    mail have been depleated not in use now

--- Friends-Hug/src/model/CRegistration.java
+++ Friends-Hug/src/model/CRegistration.java
@@ -23,9 +23,9 @@ public class CRegistration {
 		RegiSplitter.getInstance().returnRegiInfos(data);
 		DatabaseConnection connectionDB = new DatabaseConnection();
 		connectionDB.Connection("SELECT UserName FROM Data", s);
-		Codegenerator generator = new Codegenerator();
-		generator.generate();
-		Mail.getInstance().sendMail(RegiSplitter.getInstance().getRegiInfos()[4], generator.getCode());
+//		Codegenerator generator = new Codegenerator();
+//		generator.generate();
+//		Mail.getInstance().sendMail(RegiSplitter.getInstance().getRegiInfos()[4], generator.getCode());
 		// daten in db speichern
 	}
 }
